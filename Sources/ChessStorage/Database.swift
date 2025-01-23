0fb1e7466a1194d756038699187041ebf4812422
tomieq <tomieq@wp.pl>
2025-01-23 14:38:17 +0100

    Implemented Database

--- Sources/ChessStorage/Database.swift
+++ Sources/ChessStorage/Database.swift
@@ -12,6 +12,32 @@ public class Database {
     
     public init(absolutePath: String) throws {
         self.db = try Connection(absolutePath)
+        try? LessonGroupTable.createTable(db: db)
         try? LessonTable.createTable(db: db)
+        try? LessonMoveTable.createTable(db: db)
+    }
+    
+    public var allGroups: [LessonGroup] {
+        (try? LessonGroupTable.get(db: db)) ?? []
+    }
+    
+    public func create(group: LessonGroup) -> LessonGroup? {
+        try? LessonGroupTable.store(db: db, group: group)
+    }
+    
+    public func create(lesson: LessonInfo) -> LessonInfo? {
+        try? LessonTable.store(db: db, lesson: lesson)
+    }
+    
+    public func lessons(groupID: String) -> [LessonInfo] {
+        (try? LessonTable.get(db: db, groupID: groupID)) ?? []
+    }
+    
+    public func create(move: LessonMoveInfo) -> LessonMoveInfo? {
+        try? LessonMoveTable.store(db: db, move: move)
+    }
+    
+    public func moves(lessonID: String) -> [LessonMoveInfo] {
+        (try? LessonMoveTable.get(db: db, lessonID: lessonID)) ?? []
     }
 }
